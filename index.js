5acbe9d83edfafd580e1a6ff7888a98201c0017a
Jonathan Ankiewicz <ankiewicz84@gmail.com>
2017-01-16 13:18:51 -0800

    testing app init to get running on heroku

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express()
 var morgan = require('morgan')
+var bodyParser = require("body-parser");
 var ejs = require('ejs')
 var engine = require('ejs-mate')
 var d3 = require('d3')
 
-app.set('port', (process.env.PORT || 5000))
+// app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.engine('ejs', engine);
@@ -22,6 +24,10 @@ app.get('/scoreboard', function(req, res) {
   res.render('./scoreboard')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
